92a1f3f803bbe32c91aa247c2e6761e88db93d9b
ekalkur <kapil.k.kumar@ericsson.com>
2013-11-11 17:07:13 +0000

    code refactoring

--- jboss-blank-webapp/src/main/java/com/kapx/jboss/javaee6/ejb/TimerServiceBean.java
+++ jboss-blank-webapp/src/main/java/com/kapx/jboss/javaee6/ejb/TimerServiceBean.java
@@ -1,17 +1,19 @@
 package com.kapx.jboss.javaee6.ejb;
 
-import javax.ejb.*;
+import javax.ejb.Schedule;
+import javax.ejb.Schedules;
+import javax.ejb.Singleton;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
 public class TimerServiceBean {
-    private static final Logger LOG = LoggerFactory.getLogger(TimerServiceBean.class.getName());
+	private static final Logger LOG = LoggerFactory.getLogger(TimerServiceBean.class.getName());
 
-    @Schedules({ @Schedule(hour = "*", minute = "*", second = "5") })
-    public void startTimer() {
-        LOG.info("@Schedule startTimer...");
-    }
+	@Schedules({ @Schedule(hour = "*", minute = "*", second = "5") })
+	public void startTimer() {
+		LOG.info("@Schedule startTimer...");
+	}
 
 }
--- jboss-blank-webapp/src/test/java/com/kapx/jboss/javaee6/ejb/HelloWorldBeanTest.java
+++ jboss-blank-webapp/src/test/java/com/kapx/jboss/javaee6/ejb/HelloWorldBeanTest.java
@@ -2,7 +2,11 @@ package com.kapx.jboss.javaee6.ejb;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Properties;
+
 import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -17,24 +21,52 @@ import com.kapx.jboss.javaee6.interceptor.LoggingInterceptor;
 
 @RunWith(Arquillian.class)
 public class HelloWorldBeanTest {
-    @Deployment(name = "jboss-javaee6-war")
-    public static Archive<?> createTestArchive() {
-        final WebArchive war = ShrinkWrap.create(WebArchive.class, "jboss-javaee6-war.war");
-        war.addClass(HelloWorldLocal.class);
-        war.addClass(HelloWorldRemote.class);
-        war.addClass(HelloWorldBean.class);
-        war.addClass(LoggingInterceptor.class);
-        war.addAsWebInfResource("META-INF/beans.xml");
-        return war;
-    }
+	@Deployment(name = "jboss-javaee6-war")
+	public static Archive<?> createTestArchive() {
+		final WebArchive war = ShrinkWrap.create(WebArchive.class, "jboss-javaee6-webapp.war");
+		war.addClass(HelloWorldLocal.class);
+		war.addClass(HelloWorldRemote.class);
+		war.addClass(HelloWorldBean.class);
+		war.addClass(LoggingInterceptor.class);
+		war.addAsWebInfResource("META-INF/beans.xml");
+		// war.addAsWebInfResource("jboss-ejb-client.properties");
+		return war;
+	}
 
-    @EJB
-    private HelloWorldLocal helloWorldEJB;
+	@EJB(mappedName = "jboss-blank-webapp/HelloWorldBean!com.kapx.jboss.javaee6.ejb.HelloWorldRemote")
+	private HelloWorldRemote helloWorldEJB;
 
-    @Test
-    @InSequence(1)
-    public void test_sayHello() {
-        String actual = helloWorldEJB.sayHello("Kapx");
-        assertEquals("Hello !!! Kapx", actual);
-    }
+	@Test
+	@InSequence(1)
+	public void test_sayHello() {
+		// HelloWorldRemote helloWorldEJB = getHelloWorldRemote();
+		String actual = helloWorldEJB.sayHello("Kapx");
+		System.out.println("Response: " + actual);
+		assertEquals("Hello !!! Kapx", actual);
+	}
+
+	private static HelloWorldRemote getHelloWorldRemote() {
+		HelloWorldRemote remote = null;
+		try {
+			final Properties ejbProperties = new Properties();
+			ejbProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+			ejbProperties.put(Context.PROVIDER_URL, "remote://localhost:4447");
+			ejbProperties.put(Context.SECURITY_PRINCIPAL, "admin");
+			ejbProperties.put(Context.SECURITY_CREDENTIALS, "p@ssw0rd");
+			ejbProperties.put("jboss.naming.client.ejb.context", true);
+			final Context context = new InitialContext(ejbProperties);
+			remote = (HelloWorldRemote) context.lookup(getJndiName());
+			context.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return remote;
+	}
+
+	private static String getJndiName() {
+		final String appName = "jboss-javaee6-webapp";
+		final String beanName = "HelloWorldBean";
+		return appName + "/" + beanName + "!" + HelloWorldRemote.class.getName();
+	}
 }
